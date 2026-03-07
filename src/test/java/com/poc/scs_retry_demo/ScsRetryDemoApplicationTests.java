7364992b0acfbdb8e078ef610fe388bb81834b8a
Guilherme Lodi <guilherme.tlodi@gmail.com>
2026-03-07 15:45:25 -0300

    init repo

---
+++ src/test/java/com/poc/scs_retry_demo/ScsRetryDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.scs_retry_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScsRetryDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
