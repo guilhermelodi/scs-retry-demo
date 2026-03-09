1f54d79197c9b6f5c3479664663fa30d4668247f
Guilherme Lodi <34727151+guilhermelodi@users.noreply.github.com>
2026-03-09 10:13:44 -0300

    feat: lança exceção quando status do evento for ERROR

--- src/main/java/com/poc/scsretrydemo/OrderCreatedConsumer.java
+++ src/main/java/com/poc/scsretrydemo/OrderCreatedConsumer.java
@@ -12,5 +12,9 @@ public class OrderCreatedConsumer implements Consumer<OrderCreatedEvent> {
     @Override
     public void accept(OrderCreatedEvent event) {
         log.info("Mensagem consumida do tópico order-created: {}", event);
+
+        if ("ERROR".equals(event.status())) {
+            throw new RuntimeException("Falha simulada para status ERROR");
+        }
     }
 }
